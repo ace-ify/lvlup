84f8e9285fc7208f2207983a949746d598249a50
ace-ify <21naimish21@gmail.com>
2026-06-23 22:49:50 +0530

    feat: implement JWT-based authentication and user registration endpoints

--- FASTAPI/9. Capstone Project/scratchse/app/core/security.py
+++ FASTAPI/9. Capstone Project/scratchse/app/core/security.py
@@ -23,5 +23,5 @@ def create_access_token(data:dict, expires_delta: Union[timedelta,None]=None):
         expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
 
     to_encode.update({"exp":expire})
-    encoded_jwt = jwt.encode(to_encode,settings.secret_key,algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode,settings.jwt_secret_key,algorithm=ALGORITHM)
     return encoded_jwt
--- FASTAPI/9. Capstone Project/scratchse/app/main.py
+++ FASTAPI/9. Capstone Project/scratchse/app/main.py
@@ -4,6 +4,8 @@ from app.db.database import engine, Base, SessionLocal
 from app.db import models
 from app import schemas
 from app.core import security
+from fastapi.security import OAuth2PasswordRequestForm
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -38,3 +40,21 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     
     return new_user
+
+@app.post("/login", response_model=schemas.Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), 
+    db: Session = Depends(get_db)
+):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    
+    if not user or not security.verfy_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token = security.create_access_token(data={"sub": user.username})
+    
+    return {"access_token": access_token, "token_type": "bearer"}
